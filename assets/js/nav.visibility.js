f6531051c69d4dbe55b8c6d4a9dfe8862cfa3330
Yoann Poupart <66315201+Xmaster6y@users.noreply.github.com>
2025-09-19 17:52:15 +0200

    nav visible by default

--- assets/js/nav.visibility.js
+++ assets/js/nav.visibility.js
@@ -5,13 +5,13 @@
     menu_icon.addEventListener('click', function(e) {
         e.preventDefault();
         var nav = document.getElementById('menu');
-        if (nav.style.visibility == 'visible') {
-            nav.style.visibility = 'hidden';
-            menu_icon.classList.remove('menu-active');
-        }
-        else {
+        if (nav.style.visibility == 'hidden') {
             nav.style.visibility = 'visible';
             menu_icon.classList.add('menu-active');
+        }
+        else {
+            nav.style.visibility = 'hidden';
+            menu_icon.classList.remove('menu-active');
         }
         
     }, false);
